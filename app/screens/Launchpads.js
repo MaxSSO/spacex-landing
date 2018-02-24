125bb76a1e09162fff7700df26df1b88aab03908
Max <max.san.suarez.o@gmail.com>
2018-02-24 10:11:20 -0500

    add: screen basico para Launchpads.

---
+++ app/screens/Launchpads.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Launchpads extends Component {
+  static navigationOptions = {
+    title: 'Launchpads',
+  };
+
+  render() {
+    return (
+      <Text>Launchpads Component!</Text>
+    );
+  }
+}
