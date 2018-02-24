edebbfd6d9b170c4cd9a1c8f3f8821f04771f17e
Max <max.san.suarez.o@gmail.com>
2018-02-24 10:10:44 -0500

    add: screen basico para Capsules.

---
+++ app/screens/Capsules.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Capsules extends Component {
+  static navigationOptions = {
+    title: 'Capsules',
+  };
+
+  render() {
+    return (
+      <Text>Capsules Component!</Text>
+    );
+  }
+}
