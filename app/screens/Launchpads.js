8cb52b142c6106e163e02a27f9c2ab85388e702e
Max <max.san.suarez.o@gmail.com>
2018-02-24 14:05:42 -0500

    update: screen Launchpads.

--- app/screens/Launchpads.js
+++ app/screens/Launchpads.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+
+import Subheader from '../components/Subheader';
+import LaunchpadItem from '../components/LaunchpadItem';
 
 export default class Launchpads extends Component {
   static navigationOptions = {
     title: 'Launchpads',
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      launchpads: null
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://api.spacexdata.com/v2/launchpads');
+    const launchpads = await response.json();
+    this.setState({ isLoading: false, launchpads });
+  }
+
   render() {
+    if(this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
-      <Text>Launchpads Component!</Text>
+      <View style={styles.container}>
+        <Subheader text={`launchpads ${this.state.launchpads.length}`}/>
+        <FlatList
+          data={this.state.launchpads}
+          renderItem={({item}) => (
+            <LaunchpadItem 
+              name={item.location.name} 
+              details={item.details}
+              status={item.status}
+              vehiclesLaunched={item.vehicles_launched}
+              region={item.location.region}
+            />
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
